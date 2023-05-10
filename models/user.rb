306b2640d319e803b3c581314b0898e861a456cb
MisterGena <genarosalomone@gmail.com>
2023-05-10 20:33:31 -0300

    validaciones de usuario

--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
 end
 
 
