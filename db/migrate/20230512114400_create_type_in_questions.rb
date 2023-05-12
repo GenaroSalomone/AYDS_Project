a1e0def406cfe632d624c5dcf7350ff5da88895b
MisterGena <genarosalomone@gmail.com>
2023-05-12 11:12:55 -0300

    creada la columna type en question para la herencia

---
+++ db/migrate/20230512114400_create_type_in_questions.rb
@@ -0,0 +1,5 @@
+class CreateTypeInQuestions < ActiveRecord::Migration[7.0]
+  def change
+    add_column :questions, :type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_235517) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_114400) do
   create_table "answers", force: :cascade do |t|
     t.string "texto"
     t.boolean "esCorrecta"
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_235517) do
     t.string "texto"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "true_falses", force: :cascade do |t|
