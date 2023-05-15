5e991bd78ee2d4f0cb70b4cf803a89b1d950c1a1
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-05-15 08:01:44 -0300

    Se están creando dos columnas en la tabla answers que ya existen en la misma. Me da error

--- db/migrate/20230511161043_add_timestamps_to_answers.rb
+++ db/migrate/20230511161043_add_timestamps_to_answers.rb
@@ -1,6 +1,10 @@
 class AddTimestampsToAnswers < ActiveRecord::Migration[6.1]
   def change
-    add_column :answers, :created_at, :datetime, null: false
-    add_column :answers, :updated_at, :datetime, null: false
+    #add_column :answers, :created_at, :datetime, null: false
+    #add_column :answers, :updated_at, :datetime, null: false
   end
 end
+
+# ArgumentError: you can't define an already defined column 'created_at'
+# Se estan agregando dos columnas (created_at y update_at) a la tabla answers 
+# que ya existen en la tabla.
