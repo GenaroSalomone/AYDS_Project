074e3ca19c3efb19822fe0c928f4ef0ba4c1e0a8
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-05-25 22:19:08 -0300

    modif de un msj

--- server.rb
+++ server.rb
@@ -71,14 +71,14 @@ class App < Sinatra::Application
     # Crear un nuevo registro en la base de datos
       user = User.create(username: username, email: email, password: password)
       if user.save
-        @message = "¡Comience a realizar la trivia!"
+        @message = "Vuelva a logearse por favor, valla a inicio de sesión."
         erb :register_success
       else
         @error_message = "Hubo un error al registrar el usuario: #{user.errors.full_messages.join(', ')}"
         erb :message
       end
     else
-      @error_message = "Las contraseñas no coinciden"
+      @error_message = "Las contraseñas no coinciden."
       erb :message
     end
    end
@@ -99,7 +99,7 @@ class App < Sinatra::Application
       # Redirigir al usuario a una página protegida
       redirect '/protected_page'
     else
-      @error_message = "Nombre de usuario o contraseña incorrectos"
+      @error_message = "Usuario o contraseña incorrectos."
       erb :message
     end
   end
@@ -125,7 +125,7 @@ class App < Sinatra::Application
     @trivia = trivia
     associated_questions = trivia.questions
     first_question = associated_questions.first
-    @question = first_question if first_question.present?
+    @question = first_question if first_question.present? # @question recibe la pregunta n
     if first_question.present?
       @answers = Answer.where(question_id: first_question.id)
       @answer_data = {
@@ -202,12 +202,6 @@ class App < Sinatra::Application
       erb :message
     end
   end
-
-
-
-
-
-
 
 end
 # Start the server using rackup
