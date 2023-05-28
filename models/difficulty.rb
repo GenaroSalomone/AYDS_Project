46e6421022568bdfbd61e7abe118dce432be21ca
MisterGena <genarosalomone@gmail.com>
2023-05-28 09:28:21 -0300

    modelos actualizados para que funcione TF mediante herencia

--- models/difficulty.rb
+++ models/difficulty.rb
@@ -3,6 +3,7 @@ class Difficulty < ActiveRecord::Base
   has_many :trivias
   has_many :questions
   has_many :choices
+  has_many :true_falses
 
   enumerize :level, in: [:beginner, :difficult], default: :beginner
 end
--- models/true_false.rb
+++ models/true_false.rb
@@ -1,15 +1,7 @@
-class True_False < ActiveRecord::Base
-  # Relación con la tabla de preguntas
-  belongs_to :question
-
-  #Relacion con respuesta
-  has_many :answers
-
-  validate :must_have_two_answers
-
-  private
-
-  def must_have_two_answers
-    errors.add(:answers, "must have exactly 2 answers") unless answers.size == 2
-  end
+class True_False < Question
+  has_many :question_answers
+  has_many :answers, as: :question, through: :question_answers
+  belongs_to :difficulty
 end
+
+
