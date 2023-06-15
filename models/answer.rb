d99bfd436d1ede42c9c89cc58b1dc112fd2e090e
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-06-15 07:55:10 -0300

    Validaciones para test de tabla answer

--- models/answer.rb
+++ models/answer.rb
@@ -4,4 +4,9 @@ class Answer < ActiveRecord::Base
   has_many :questions, through: :question_answers
   #indicacion a active record para que reconozca la columna como arreglo
   serialize :answers_autocomplete, JSON
+
+  #validaciones para pruebas unitarias
+  validates :question_id, presence: true
+  validates :text, presence: true
+  validates :correct, inclusion: { in: [true, false] }
 end
