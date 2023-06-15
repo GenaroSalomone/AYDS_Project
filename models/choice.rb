246dcbd8f72af755439c2ac780717c30e8bd1cb2
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-06-15 20:25:35 -0300

    Validaciones modelo choice

--- models/choice.rb
+++ models/choice.rb
@@ -2,8 +2,5 @@ class Choice < Question
   has_many :question_answers
   has_many :answers, as: :question, through: :question_answers
   belongs_to :difficulty
-  
-  # Validaciones para las pruebas
-  validates :text, presence: true
-  validates :difficulty_id, presence: true
+  validates_presence_of :text, :help, :difficulty_id
 end
