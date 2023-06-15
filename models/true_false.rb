becd93c3c6813a1658174cde33610aae03ad3011
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-06-15 20:26:15 -0300

    Validaciones en modelo true_false

--- models/true_false.rb
+++ models/true_false.rb
@@ -2,6 +2,7 @@ class True_False < Question
   has_many :question_answers
   has_many :answers, as: :question, through: :question_answers
   belongs_to :difficulty
+  validates_presence_of :text, :help, :difficulty_id
 end
 
 
