2e672f324a6e05270c8d1a55e1530524c7bd1158
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-06-14 22:42:30 -0300

    Añadidas validaciones para test

--- models/choice.rb
+++ models/choice.rb
@@ -2,4 +2,9 @@ class Choice < Question
   has_many :question_answers
   has_many :answers, as: :question, through: :question_answers
   belongs_to :difficulty
+  
+  # Validaciones para las pruebas
+  validates :text, presence: true
+  validates :difficulty_id, presence: true
+  validates :help, presence: true, if: ->(choice) { choice.difficulty&.level == :beginner }
 end
