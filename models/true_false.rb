cec84f6c51f9e84602008d41f7cd52b097d853ef
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-06-17 09:49:11 -0300

    Actualizo proyecto a como lo tenemos

--- models/true_false.rb
+++ models/true_false.rb
@@ -2,7 +2,6 @@ class True_False < Question
   has_many :question_answers
   has_many :answers, as: :question, through: :question_answers
   belongs_to :difficulty
-  validates_presence_of :text, :help, :difficulty_id
 end
 
 
