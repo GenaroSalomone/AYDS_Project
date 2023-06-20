d935a37bfa7ea54c0d7ed529028195ef580e961a
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-06-19 21:30:31 -0300

    Borradas validaciones de choice

--- models/choice.rb
+++ models/choice.rb
@@ -2,5 +2,4 @@ class Choice < Question
   has_many :question_answers
   has_many :answers, as: :question, through: :question_answers
   belongs_to :difficulty
-  validates_presence_of :text, :help, :difficulty_id
 end
