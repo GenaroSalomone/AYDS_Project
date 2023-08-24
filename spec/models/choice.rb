d430ce0b8bf92e5c4e2c11a5dc0db6c21d19f716
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-08-24 14:00:43 -0300

    Carpeta equivocada para choice.rb

--- spec/models/choice.rb
+++
@@ -1,7 +0,0 @@
-class Choice < Question
-  has_many :question_answers
-  has_many :answers, as: :question, through: :question_answers
-  belongs_to :difficulty
-  validates :text, presence: true
-  validates :difficulty_id, presence: true
-end
