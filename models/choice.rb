5677e8ba134ee728d69e69a7d1cb6c378dfa82f9
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-08-24 14:01:45 -0300

    Testing para modelo Choice

--- models/choice.rb
+++ models/choice.rb
@@ -2,4 +2,6 @@ class Choice < Question
   has_many :question_answers
   has_many :answers, as: :question, through: :question_answers
   belongs_to :difficulty
+  validates :text, presence: true
+  validates :difficulty_id, presence: true
 end
