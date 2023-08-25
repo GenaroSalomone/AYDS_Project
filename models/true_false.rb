c6288f08f87b8376b49e823e90d0ce8ccb2d75d8
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-08-25 13:28:36 -0300

    Validaciones en modelos true_false y autocomplete

--- models/true_false.rb
+++ models/true_false.rb
@@ -2,6 +2,8 @@ class True_False < Question
   has_many :question_answers
   has_many :answers, as: :question, through: :question_answers
   belongs_to :difficulty
+  validates :text, presence: true
+  validates :difficulty_id, presence: true
 end
 
 
