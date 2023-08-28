dbd35f4376a87f43fa9f29641d025192925f92fa
MisterGena <genarosalomone@gmail.com>
2023-08-28 10:30:53 -0300

    Test Suite para QuestionAnswer

--- models/question_answer.rb
+++ models/question_answer.rb
@@ -4,4 +4,28 @@ class QuestionAnswer < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
   belongs_to :trivia
+
+  validates :question_id, uniqueness: { scope: [:answer_id, :trivia_id] }
+
+  validate :consistent_difficulty
+  validate :consistent_combination
+
+  private
+
+  def consistent_combination
+    if question_id.blank? || answer_id.blank? || trivia_id.blank?
+      errors.add(:base, "Question, answer, and trivia must be present")
+    end
+  end
+
+  private
+
+  def consistent_difficulty
+    if question && answer && trivia
+      unless question.difficulty.level == trivia.difficulty.level
+        errors.add(:base, "Question and trivia must have the same difficulty")
+      end
+    end
+  end
+
 end
---
+++ spec/models/question_answer_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../../models/init.rb'
+require 'sinatra/activerecord'
+
+describe QuestionAnswer do
+
+  describe 'validations' do
+    let(:user) { User.new }
+    let(:difficult_difficulty) { Difficulty.create!(level: "difficult") }
+    let(:question) { Question.create!(text: 'question', difficulty: difficult_difficulty) }
+    let(:answer) { Answer.create!(question: question, text: 'text', correct: true) }
+    let(:trivia) { Trivia.create!(user: user, difficulty: difficult_difficulty) }
+
+    it "validates uniqueness of question_id, answer_id, and trivia_id combination" do
+      QuestionAnswer.create!(question_id: question.id, answer_id: answer.id, trivia_id: trivia.id)
+      duplicate_question_answer = QuestionAnswer.new(question_id: question.id, answer_id: answer.id, trivia_id: trivia.id)
+      expect(duplicate_question_answer).not_to be_valid
+    end
+
+    it "validates presence of associated question, answer, and trivia" do
+      question_answer = QuestionAnswer.new
+      expect(question_answer).not_to be_valid
+      expect(question_answer.errors[:base]).to include("Question, answer, and trivia must be present")
+    end
+
+    it "validates that question and trivia have the same difficulty" do
+      inconsistent_trivia = Trivia.create!(user: user, difficulty: Difficulty.create!(level: "beginner"))
+      inconsistent_question_answer = QuestionAnswer.new(question: question, answer: answer, trivia: inconsistent_trivia)
+      expect(inconsistent_question_answer).not_to be_valid
+      expect(inconsistent_question_answer.errors[:base]).to include("Question and trivia must have the same difficulty")
+    end
+
+    it "validates that question and trivia with the same difficulty are valid" do
+      consistent_question_answer = QuestionAnswer.new(question: question, answer: answer, trivia: trivia)
+      expect(consistent_question_answer).to be_valid
+    end
+  end
+
+end
