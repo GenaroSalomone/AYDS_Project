6ad7ab40fc80af24ef7253e9fd3f78a7bc661715
MisterGena <genarosalomone@gmail.com>
2023-08-28 18:56:11 -0300

    Solucion al problema que da la validacion activeRecord para answer en la app

--- models/answer.rb
+++ models/answer.rb
@@ -6,7 +6,14 @@ class Answer < ActiveRecord::Base
   serialize :answers_autocomplete, JSON
 
   validates :question_id, presence: true
-  validates :text, presence: true
-  validates :correct, inclusion: { in: [true, false] }
-  validates :text, length: { maximum: 255 }
+  validates :text, presence: true, unless: :autocomplete_question?
+  validates :correct, inclusion: { in: [true, false] }, unless: :autocomplete_question?
+  validates :text, length: { maximum: 255 }, unless: :autocomplete_question?
+
+  private
+
+  def autocomplete_question?
+    # Verifica si la pregunta asociada es de tipo Autocomplete
+    question.is_a?(Autocomplete)
+  end
 end
