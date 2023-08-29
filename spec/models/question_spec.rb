ad0c8a0e3a32bc93f26ff918517dd5030df03c3f
MisterGena <genarosalomone@gmail.com>
2023-08-28 22:30:43 -0300

    Question_Spec

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../../models/init.rb'
+require 'sinatra/activerecord'
+
+describe Question do
+  describe 'validations' do
+    let(:user) { User.new }
+    let(:difficult_difficulty) { Difficulty.create!(level: "difficult") }
+
+    it 'is valid when belongs to a subclass' do
+      choice_= Choice.create!( text: "¿Qué es un compilador?", difficulty: difficult_difficulty )
+      question = Question.create!( choice.attributes )
+      expect(question).to be_valid
+    end
+
+    it 'is invalid when not belongs to a subclass' do
+      question = Question.new
+      expect(question).to be_invalid
+      expect(question.errors[:base]).to include('Question must belong to a subclass')
+    end
+  end
+end
