149ace851902b0bae77adeb1847c394a8534acaf
MisterGena <genarosalomone@gmail.com>
2023-08-29 11:40:36 -0300

    Validacion Score para Ranking

--- models/ranking.rb
+++ models/ranking.rb
@@ -2,7 +2,20 @@ class Ranking < ActiveRecord::Base
   belongs_to :user
   belongs_to :trivia
   belongs_to :difficulty
+
   validates :user_id, presence: true
   validates :difficulty_id, presence: true
   validates :score, presence: true
+
+  #El score estara necesariamente entre 0 y 100
+  validate :score_must_be_between_0_and_100
+
+  private
+
+  def score_must_be_between_0_and_100
+    if score.present? && (score < 0 || score > 100)
+      errors.add(:score, "must be between 0 and 100")
+    end
+  end
+
 end
--- spec/models/ranking_spec.rb
+++ spec/models/ranking_spec.rb
@@ -5,7 +5,7 @@ require 'sinatra/activerecord'
 describe Ranking do
 
   describe 'validations' do
-    
+
     it "is invalid without a user ID" do
       ranking = Ranking.new(score: 0, difficulty_id: 1)
       expect(ranking.valid?).to eq(false)
@@ -14,17 +14,34 @@ describe Ranking do
     it "is invalid without a difficulty ID" do
       ranking = Ranking.new(user_id: 1, score: 0)
       expect(ranking.valid?).to eq(false)
-    end  
-    
+    end
+
     it "is valid with all required attributes" do
       ranking = Ranking.new(user_id: 1, score: 0, difficulty_id: 2)
       expect(ranking.valid?).to eq(true)
     end
 
+    it 'is invalid if score is less than 0' do
+      ranking = Ranking.new(user_id: 1, score: -1, difficulty_id: 2)
+      expect(ranking).to be_invalid
+      expect(ranking.errors[:score]).to include("must be between 0 and 100")
+    end
+
+    it 'is invalid if score is greater than 100' do
+      ranking = Ranking.new(user_id: 1, score: 101, difficulty_id: 2)
+      expect(ranking).to be_invalid
+      expect(ranking.errors[:score]).to include("must be between 0 and 100")
+    end
+
+    it 'is valid if score is between 0 and 100' do
+      ranking = Ranking.new(user_id: 1, score: 50, difficulty_id: 2)
+      expect(ranking).to be_valid
+    end
+
   end
 
   describe 'associations' do
-    
+
     it "belongs to user" do
       association = described_class.reflect_on_association(:user)
       expect(association.macro).to eq(:belongs_to)
