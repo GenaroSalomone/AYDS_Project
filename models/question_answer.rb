e4e60314c5ae3477170f4ad078a59212df8cfd21
MisterGena <genarosalomone@gmail.com>
2023-08-28 21:38:32 -0300

    Solucionado validacion active record en QA que rompia la app

--- models/question_answer.rb
+++ models/question_answer.rb
@@ -5,27 +5,28 @@ class QuestionAnswer < ActiveRecord::Base
   belongs_to :answer
   belongs_to :trivia
 
+  #no puede haber tuplas iguales con question_id, answer_id, trivia_id
   validates :question_id, uniqueness: { scope: [:answer_id, :trivia_id] }
-
-  validate :consistent_difficulty
   validate :consistent_combination
+  validate :consistent_difficulty
 
   private
 
-  def consistent_combination
-    if question_id.blank? || answer_id.blank? || trivia_id.blank?
-      errors.add(:base, "Question, answer, and trivia must be present")
-    end
-  end
-
-  private
-
+  #una question answer debe tener el mismo nivel de dificultad para la question y la trivia
   def consistent_difficulty
-    if question && answer && trivia
+    if question && trivia
       unless question.difficulty.level == trivia.difficulty.level
         errors.add(:base, "Question and trivia must have the same difficulty")
       end
     end
   end
 
+  private
+
+  #una question answer no puede tener nil en question y trivia simultaneamente
+  def consistent_combination
+    if question.blank? || trivia.blank?
+      errors.add(:base, "Question, answer, and trivia must be present")
+    end
+  end
 end
