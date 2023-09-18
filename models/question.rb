d5c199786bad68782cd4fb0d91be66350994ff20
Genaro Salomone <genarosalomone@gmail.com>
2023-09-18 13:43:04 -0300

    Corrected Question Validation

--- models/question.rb
+++ models/question.rb
@@ -12,7 +12,7 @@ class Question < ActiveRecord::Base
 
   #una question debe ser necesariamente una subclase de question (no puede haber Questions que no sean subclase de Question)
   def must_belong_to_subclass
-    unless self.is_a?(Autocomplete) || self.is_a?(True_False) || self.is_a?(Choice) || self.is_a?(Translated_Question)
+    unless self.is_a?(Autocomplete) || self.is_a?(True_False) || self.is_a?(Choice)
       errors.add(:base, 'Question must belong to a subclass')
     end
   end
